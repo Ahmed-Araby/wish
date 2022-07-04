6e684cc0b62a7d9c43ffd06e20532d8db7c03b75
Ahmed Araby <ahmedaraby605@gmail.com>
2022-07-04 23:00:44 +0200

    implement built in function 'cd'

--- built-in-funs.c
+++ built-in-funs.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "utils.h"
+#include "shared.h"
+
+extern char* cwd;
+extern int cwdl;
 
 void myexit(struct program* p){
     exit(0);
 }
+
 void cd(struct program* p){
-    printf("built-in cd function not implemented yet \n");
+    if(cwd == NULL){
+        printf("error: cwd is null");
+        exit(1);
+    }
+    if(p->argc != 1){
+        printf("error: invalid number of arguments \n");
+        return;
+    }
+    int ret = chdir(p->argv[0]);
+    if(ret == -1)
+        printf("error: failed to change the cwd \n");
+    else 
+        getcwd(cwd, cwdl);
 }
+
 void path(struct program* p){
     printf("build-in path function not implemented yet \n");
 }
